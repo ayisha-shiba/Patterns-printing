71cd97e076ff7648ed8c11556a41aa2ea837e0b1
ayisha-shiba <ayishashiba.p@gmail.com>
2025-11-24 23:08:49 +0530

    Added number pattern

--- number_patterns.c
+++ number_patterns.c
@@ -19,3 +19,24 @@ int main()
 // 1 2 3 2 1
 // 1 2 3 4 3 2 1
 // 1 2 3 4 5 4 3 2 1
+#include <stdio.h>
+
+int main()
+{   int i,j;
+    int num;
+    for(i=1;i<=5;i++){
+        num =1;
+        for(j=5;j>=i;j--){
+            printf(" %d",num);
+            num += 2;
+        }
+
+     printf("\n");
+    }
+    return 0;
+}
+// 1 3 5 7 9
+// 1 3 5 7
+// 1 3 5
+// 1 3
+// 1
