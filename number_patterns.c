04460887d2f8191b306aefc1881d85e88f950149
ayisha-shiba <ayishashiba.p@gmail.com>
2025-12-14 13:47:26 +0530

    Added star patterns

--- number_patterns.c
+++ number_patterns.c
@@ -17,28 +17,28 @@
 // //  1 2 3 4
 // //  1 2 3 4 5
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
-{   int i,j;
-    for(i=1;i<=5;i++){
-        for(j=1;j<=i;j++){
-            if(i%2==0){
-                printf("0 ");
-            }else{
-                printf("1 ");
-            }
-        }printf("\n");
-    }
+// int main()
+// {   int i,j;
+//     for(i=1;i<=5;i++){
+//         for(j=1;j<=i;j++){
+//             if(i%2==0){
+//                 printf("0 ");
+//             }else{
+//                 printf("1 ");
+//             }
+//         }printf("\n");
+//     }
    
-    return 0;
-}
+//     return 0;
+// }
 
-// 1
-// 0 0
-// 1 1 1 
-// 0 0 0 0
-// 1 1 1 1 1
+// // 1
+// // 0 0
+// // 1 1 1 
+// // 0 0 0 0
+// // 1 1 1 1 1
 
 
 
--- star_patterns.c
+++ star_patterns.c
@@ -58,4 +58,27 @@
 //  * * *
 //  * *
 //  *
+
+#include <stdio.h>
+
+int main()
+{   int i,j; 
+    int col = 12;
+    for(i=1; i<=4 ; i++){
+        for(j = 1 ; j<=i*3 ; j++){
+            printf(" *");
+        }
+    printf("\n");
+}
+    return 0;
+}
+
+
+
+
+
+// * * *
+// * * * * * *
+// * * * * * * * * *
+// * * * * * * * * * * * *
  
